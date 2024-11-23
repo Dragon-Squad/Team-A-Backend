35f6649c363d82c559781cb47975eefa20c25fed
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-11-23 18:38:34 +0200

    feat: create project

--- microservices/ProjectManagementService/Project/ProjectController.js
+++ microservices/ProjectManagementService/Project/ProjectController.js
@@ -1,134 +1,14 @@
-const ProjectService = require('./ProjectService');
+const ProjectService = require("./ProjectService");
 
 class ProjectController {
-  // Method to get all projects 
-  async getAllProjects(req, res) {
+  // Create a Project
+  async create(req, res) {
     try {
-      const page = parseInt(req.query.page) || 1; 
-      const limit = parseInt(req.query.limit) || 10; 
-      const filters = {
-        month: req.query.month,
-        region: req.query.region,
-        country: req.query.country,
-        category: req.query.category,
-        search: req.query.search
-      };
-      const donorCountry = req.query.donorCountry || 'Vietnam';
-
-      const projects = await ProjectService.getAllProjects(page, limit, filters, donorCountry);
-      return res.status(200).json(projects);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  async getActiveProjects(req, res) {
-    try {
-      console.log("Controller Executes");
-      const page = parseInt(req.query.page) || 1; 
-      const limit = parseInt(req.query.limit) || 10; 
-      const filters = {
-        month: req.query.month,
-        region: req.query.region,
-        country: req.query.country,
-        category: req.query.category,
-        search: req.query.search
-      };
-      const donorCountry = req.query.donorCountry || 'Vietnam';
-      const projects = await ProjectService.getActiveProjects(page, limit, filters, donorCountry);
-      return res.status(200).json(projects);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method to get a project by ID
-  async getProjectById(req, res) {
-    try {
-      const projectId = req.params.id; 
-      const project = await ProjectService.getProjectById(projectId);
-      return res.status(200).json(project);
-    } catch (err) {
-      return res.status(404).json({ error: err.message });
-    }
-  }
-
-  async getProjectsByCharity(req, res) {
-    try {
-      const userId = req.params.id; 
-      const page = parseInt(req.query.page) || 1; 
-      const limit = parseInt(req.query.limit) || 10; 
-      getProjectsByCharity
-      const results = await ProjectService.getMyProjects(userId, page, limit);
-      return res.status(200).json(results);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method to create a new project
-  async createProject(req, res) {
-    try {
-      const projectData = req.body; 
-      const role = req.role; 
-      const userId = req.id; 
-
-      const newProject = await ProjectService.createProject(projectData, role, userId);
-      return res.status(201).json({ message: 'Project created successfully', project: newProject });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method to update a project
-  async updateProject(req, res) {
-    try {
-      const projectId = req.params.id; 
-      const projectData = req.body; 
-      const userId = req.id; 
-      const role = req.role; 
-
-      const updatedProject = await ProjectService.updateProject(projectId, projectData, userId, role);
-      return res.status(200).json({ message: 'Project updated successfully', project: updatedProject });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method for admin to activate a pending project
-  async activateProject(req, res) {
-    try {
-      const projectId = req.params.id; 
-      await ProjectService.activateProject(projectId);
-      return res.status(200).json({ message: 'Project activated successfully' });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method for charity owner to halt an active project
-  async haltProject(req, res) {
-    try {
-      const projectId = req.params.id; 
-      const userId = req.id; 
-
-      await ProjectService.haltProject(projectId, userId);
-      return res.status(200).json({ message: 'Project halted successfully' });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  // Method for admin to delete a project
-  async deleteProject(req, res) {
-    try {
-      const projectId = req.params.id; 
-      const role = req.role;
-      const userId = req.id;
-      await ProjectService.deleteProject(projectId, role, userId);
-      return res.status(200).json({ message: 'Project deleted successfully' });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
+      const projectData = req.body;
+      const newProject = await ProjectService.create(projectData);
+      res.status(201).json(newProject);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
   }
 }
--- microservices/ProjectManagementService/Project/ProjectRouter.js
+++ microservices/ProjectManagementService/Project/ProjectRouter.js
@@ -1,68 +1,14 @@
-const express = require('express');
-const ProjectController = require('./ProjectController');
+const express = require("express");
+const ProjectController = require("./ProjectController");
 const { authenticate, authorize } = require("../middleware/auth");
-const UserType = require('../enum/UserType');
+const UserType = require("../enum/UserType");
 const ProjectRouter = express.Router();
 
-ProjectRouter.get(
-    '/all', 
-    authenticate,
-    authorize([UserType.ADMIN, UserType.DONOR]),
-    ProjectController.getAllProjects
-);
-
-ProjectRouter.get(
-    '/active', 
-    ProjectController.getActiveProjects
-);
-
-ProjectRouter.get(
-    '/:id', 
-    authenticate,
-    authorize([UserType.ADMIN, UserType.DONOR, UserType.CHARITY]),
-    ProjectController.getProjectById
-);
-
-ProjectRouter.get(
-    '/charity/:id', 
-    authenticate,
-    authorize([UserType.DONOR, UserType.CHARITY]),
-    ProjectController.getProjectsByCharity
-);
-
 ProjectRouter.post(
-    '/new', 
-    authenticate,
-    authorize([UserType.ADMIN, UserType.CHARITY]),
-    ProjectController.createProject
-);
-
-ProjectRouter.put(
-    '/:id',
-    authenticate,
-    authorize([UserType.ADMIN, UserType.CHARITY]),
-    ProjectController.updateProject
-);
-
-ProjectRouter.patch(
-    '/:id/active',
-    authenticate,
-    authorize([UserType.ADMIN]), 
-    ProjectController.activateProject
-);
-
-ProjectRouter.patch(
-    '/:id/halt',
-    authenticate,
-    authorize([UserType.CHARITY]), 
-    ProjectController.haltProject
-);
-
-ProjectRouter.delete(
-    '/:id',
-    authenticate,
-    authorize([UserType.ADMIN, UserType.CHARITY]),
-    ProjectController.deleteProject
+  "/",
+  //   authenticate,
+  //   authorize([UserType.ADMIN, UserType.CHARITY]),
+  ProjectController.create
 );
 
 module.exports = ProjectRouter;
