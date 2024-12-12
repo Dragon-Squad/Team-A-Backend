b7480172565ac1cfceefed14dd608dd8e9870cee
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-12-12 15:43:29 +0700

    feat: request fields validation in createProject

--- microservices/ProjectManagementService/Project/ProjectService.js
+++ microservices/ProjectManagementService/Project/ProjectService.js
@@ -1,9 +1,59 @@
+const validators = require("../utils/requestValidators");
 const ProjectRepository = require("./ProjectRepository");
 const ProjectValidator = require("./ProjectValidator");
 const MessageBroker = require("../broker/MessageBroker");
 
 class ProjectService {
   async create(projectData) {
+    const errors = [];
+
+    // Use validation helpers
+    validators.isRequired(projectData.charityId, "charityId", errors);
+    validators.isString(projectData.charityId, "charityId", errors);
+
+    validators.isRequired(projectData.categoryId, "categoryId", errors);
+    validators.isString(projectData.categoryId, "categoryId", errors);
+
+    validators.isRequired(projectData.regionId, "regionId", errors);
+    validators.isString(projectData.regionId, "regionId", errors);
+
+    validators.isRequired(projectData.title, "title", errors);
+    validators.isString(projectData.title, "title", errors);
+
+    validators.isString(projectData.description, "description", errors);
+
+    validators.isRequired(projectData.goalAmount, "goalAmount", errors);
+    validators.isPositiveNumber(projectData.goalAmount, "goalAmount", errors);
+
+    if (typeof projectData.raisedAmount !== "undefined") {
+      validators.isNonNegativeNumber(
+        projectData.raisedAmount,
+        "raisedAmount",
+        errors
+      );
+    }
+
+    validators.isRequired(projectData.status, "status", errors);
+    validators.isEnumValue(
+      projectData.status,
+      "status",
+      ["pending", "active", "halted", "closed"],
+      errors
+    );
+
+    validators.isValidDate(projectData.endDate, "endDate", errors);
+
+    validators.isArrayOfStrings(projectData.images, "images", errors);
+    validators.isArrayOfStrings(projectData.videos, "videos", errors);
+
+    validators.isString(projectData.account, "account", errors);
+
+    // Throw if there are errors
+    if (errors.length > 0) {
+      throw new Error(`Validation error: ${errors.join(" ")}`);
+    }
+
+    // Proceed with project creation
     return await ProjectRepository.create(projectData);
   }
 
