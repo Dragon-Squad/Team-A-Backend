4f6688a103fb28d9cf2ead97feee55cc885ddd1e
shirin44 <S3983427@rmit.edu.vn>
2025-01-06 14:48:17 +0700

    fixing run.sh

--- run.sh
+++ run.sh
@@ -5,19 +5,20 @@ npm install
 node setup.js
 
 echo ">> Starting Microservices Docker containers..."
-cd ..
+cd ../
 docker-compose up --build -d
 
 echo ">> Registering servers..."
-node microservices/Setup/serversRegister.js
+node Setup/serversRegister.js
 
 echo "================== Kong ======================="
 echo ">> Starting Kong Docker containers..."
+cd ../
 docker-compose -f gateway/docker-compose.yml up -d --build
 
 echo ">> Checking server health..."
 for i in {1..10}; do
-  if curl -s http://localhost:8003/health/check | grep -q "Server is working!"; then
+  if curl -s http://localhost:8803/health/check | grep -q "Server is working!"; then
     echo ">> Server is ready!"
     break
   fi
@@ -26,7 +27,7 @@ for i in {1..10}; do
 done
 
 # If the server isn't ready after 10 attempts, exit with an error
-if ! curl -s http://localhost:8003/health/check | grep -q "Server is working!"; then
+if ! curl -s http://localhost:8803/health/check | grep -q "Server is working!"; then
   echo ">> Server health check failed!" >&2
   exit 1
 fi
