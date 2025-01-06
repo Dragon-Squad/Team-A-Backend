c247dae54320293d27a6e870b75be247b2628556
Trần Thanh Tùng <s3927562@rmit.edu.vn>
2025-01-06 15:22:57 +0700

    Initial Middleware
    TODO:
    - Store keys securely
    - Wait for Team B to finish

--- microservices/ProjectManagementService/middleware/auth.js
+++ microservices/ProjectManagementService/middleware/auth.js
@@ -1,78 +1,73 @@
-const { setCookie } = require('./setCookie');
-const { introspect } = require('./introspect');
-const { getUserById } = require('../User/UserService');
-const { hasAccessToken } = require("../AccessToken/AccessTokenService");
+require("dotenv").config();
+const { getUserById } = require('../models/User/UserService');
+const { createPrivateKey } = require('crypto')
+const { jwtDecrypt } = require('jose')
+const jwt = require('jsonwebtoken');
 
 const authenticate = async (req, res, next) => {
-  const accessToken = req.cookies.accessToken; 
-  console.log("Access token: " + accessToken);
+  const accessToken = req.cookies.accessToken
+  if (!accessToken) {
+    return res.status(401).json({
+      message: "No access token"
+    });
+  }
+
+  const privateKey = createPrivateKey(process.env.JWE_PRIVATE_MW);
 
   try {
-    // Check if refresh token exists
-    if (!accessToken) {
-      return res.status(403).json("FORBIDDEN: Authentication Invalid");
-    }
+    const decrypted = await jwtDecrypt(accessToken, privateKey);
+    const jws = decrypted.payload.jws;
+    const decoded = jwt.verify(jws, process.env.JWS_PUBLIC_MW, { algorithm: 'RS256' });
 
-    // Verify refresh token
-    const payload = introspect(accessToken, process.env.ACCESS_TOKEN_SECRET);
-
-    if (!payload) {
-      return res.status(401).json("UNAUTHORIZED: Invalid token");
-    }
-
-    // Get user by role
-    const user = await getUserById(payload.id);
-    const isValid = await hasAccessToken(accessToken);
-    if (!user || !isValid ) {
-      return res.status(403).json("FORBIDDEN: Authentication Invalid");
-    }
-    
-    // Set new token cookies
-    setCookie(res, accessToken);
-
-    // Attach user information to the request
-    req.id = payload.id;
-    req.role = payload.role;
+    res.locals.id = decoded.userId;
+    res.locals.role = decoded.userRole;
 
     next();
-  } catch (err) {
-    console.error(err);
-    res
-    .status(500)
-    .json("INTERNAL SERVER ERROR");
+  } catch (error) {
+    console.log(error);
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({
+        message: "Expired access token"
+      });
+    }
+    return res.status(401).json({
+      message: "Invalid access token"
+    });
   }
-};
+}
 
 const authorize = (allowedRoles) => {
   return async (req, res, next) => {
     try {
-      const { id, role } = req;
+      const { role } = res.locals;
 
-      // Check if the current role is in the allowed roles
-      if (allowedRoles.includes(role)) {
-        const user = getUserById(id)
-
-        if (user) {
-          console.log("Grant Permission");
-
-          req.id = id;
-          req.role = role; 
-
-          return next();
-        }
+      if (!allowedRoles.includes(role)) {
+        return res.status(403).json({
+          message: "Insufficient permissions"
+        });
       }
 
-      res
-        .status(403)
-        .json("FORBIDDEN: You do not have the required permissions!");
+      const response = await fetch(`http://localhost:3010/user`, {
+        headers: {
+          "Cookie": `accessToken=${req.cookies.accessToken};refreshToken=${req.cookies.refreshToken}`
+        }
+      });
+
+      if (!response.ok) {
+        return res.status(403).json({
+          message: "User not found"
+        });
+      }
+
+      next();
     } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .json("INTERNAL SERVER ERROR");
-    }
-  };
-};
+      console.log(error);
+      return res.status(500).json({
+        message: "Authorization failed",
+      });
+    };
+  }
+}
 
 module.exports = {
   authenticate,
