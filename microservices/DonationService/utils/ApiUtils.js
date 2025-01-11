97c806ff12db57d2d128f2ae1045fa8a0dc31103
Trần Thanh Tùng <s3927562@rmit.edu.vn>
2025-01-11 14:36:41 +0700

    fix

--- microservices/DonationService/utils/ApiUtils.js
+++ microservices/DonationService/utils/ApiUtils.js
@@ -1,27 +1,27 @@
 const axios = require("axios");
 
-async function getDonor(accessToken){
+async function getDonor(accessToken) {
     const response = await axios.get(
         `http://100.112.207.9:3000/api/donors/my`,
         {
             credentials: "include",
             method: "GET",
             headers: {
-              "Content-Type": "application/json",
-              "Cookie": `accessToken=${accessToken}`,
-            }
+                "Content-Type": "application/json",
+                Cookie: `accessToken=${accessToken}`,
+            },
         }
-      );
+    );
     if (!response.data) {
         throw new Error("Error validating donor ID");
     }
     return response.data;
 }
 
-async function getUser(userId){
+async function getUser(userId) {
     const response = await axios.get(
-        `http://100.112.207.9:3000/api/users/${userId}`,
-      );
+        `http://100.112.207.9:3000/api/users/${userId}`
+    );
     if (!response.data) {
         throw new Error("Error validating user ID");
     }
--- microservices/ProjectManagementService/utils/projectServiceUtils.js
+++ microservices/ProjectManagementService/utils/projectServiceUtils.js
@@ -2,86 +2,82 @@ const CategoryService = require("../models/Category/CategoryService");
 const RegionService = require("../models/Region/RegionService");
 const axios = require("axios");
 
-const TEAM_B_BACKEND_URL = process.env.TEAM_B_BACKEND_URL || "http://100.112.207.9:3000/api";
+const TEAM_B_BACKEND_URL =
+    process.env.TEAM_B_BACKEND_URL || "http://100.112.207.9:3000/api";
 
 module.exports = {
     validateCharity: async function (charityId, accessToken) {
-      const charityResponse = await axios.get(
-        TEAM_B_BACKEND_URL + `/charities/${charityId}`,
-        {
-            credentials: "include",
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              "Cookie": `accessToken=${accessToken}`,
+        const charityResponse = await axios.get(
+            TEAM_B_BACKEND_URL + `/charities/${charityId}`,
+            {
+                credentials: "include",
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: `accessToken=${accessToken}`,
+                },
             }
-        }
-      );
-      if (!charityResponse.data) 
-        throw new Error("No Charity Found");
-      
-      return charityResponse.data;
+        );
+        if (!charityResponse.data) throw new Error("No Charity Found");
+
+        return charityResponse.data;
     },
-  
+
     validateCharityName: async function (keyword, accessToken) {
-      const charityResponse = await axios.get(
-        TEAM_B_BACKEND_URL + `/charities/search?keyword=${keyword}`,
-        {
-            credentials: "include",
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              "Cookie": `accessToken=${accessToken}`,
+        const charityResponse = await axios.get(
+            TEAM_B_BACKEND_URL + `/charities/search?keyword=${keyword}`,
+            {
+                credentials: "include",
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: `accessToken=${accessToken}`,
+                },
             }
-        }
-      );
-      if (!charityResponse.data) 
-        throw new Error("No Charity Found");
-      
-      return charityResponse.data;
+        );
+        if (!charityResponse.data) throw new Error("No Charity Found");
+
+        return charityResponse.data;
     },
-  
+
     validateUser: async function (userId) {
-      const userResponse = await axios.get(
-         TEAM_B_BACKEND_URL + `/users/${userId}`
-      );
-      if (!userResponse.data) 
-        throw new Error("No Email Found");
-      
-      return userResponse.data;
+        const userResponse = await axios.get(
+            TEAM_B_BACKEND_URL + `/users/${userId}`
+        );
+        if (!userResponse.data) throw new Error("No Email Found");
+
+        return userResponse.data;
     },
-  
+
     validateCategory: async function (categoryIds) {
-      const categories = [];
-  
-      for (const categoryId of categoryIds) {
-        const category = await CategoryService.getCategoryById(categoryId);
-        if (!category) {
-          throw new Error("Error validating category ID");
+        const categories = [];
+
+        for (const categoryId of categoryIds) {
+            const category = await CategoryService.getCategoryById(categoryId);
+            if (!category) {
+                throw new Error("Error validating category ID");
+            }
+            categories.push(category);
         }
-        categories.push(category);
-      }
-      return categories;
+        return categories;
     },
-  
+
     validateRegion: async function (regionId) {
-      const region = await RegionService.getRegionById(regionId);
-      if (!region) 
-        throw new Error("Error validating region ID");
-      
-      return region;
+        const region = await RegionService.getRegionById(regionId);
+        if (!region) throw new Error("Error validating region ID");
+
+        return region;
     },
-  
+
     mergeNotificationLists: function (region, categories) {
-      const result = new Set();
-  
-      categories.forEach((category) => {
-        category.notificationList.forEach((item) => result.add(item));
-      });
-  
-      region.notificationList.forEach((item) => result.add(item));
-  
-      return result;
-    }
-  };
-  
+        const result = new Set();
+
+        categories.forEach((category) => {
+            category.notificationList.forEach((item) => result.add(item));
+        });
+
+        region.notificationList.forEach((item) => result.add(item));
+
+        return result;
+    },
+};
