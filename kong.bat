ddd9fafd8b53cd859505599cbcb84b86662f8043
Trần Thanh Tùng <s3927562@rmit.edu.vn>
2025-01-13 14:21:56 +0700

    Update Kong setup script

--- kong.bat
+++ kong.bat
@@ -1,5 +1,8 @@
+@echo off
+
 echo ================== Kong =======================
-echo Starting Kong Docker containers...
+echo ^>^> Starting Kong Docker containers...
 docker-compose -f gateway/docker-compose.yml up -d --build
 
-echo ">> Done!"
+echo ^>^> Done!
+pause
--- kong.sh
+++ kong.sh
@@ -3,3 +3,5 @@ echo ">> Starting Kong Docker containers..."
 docker-compose -f gateway/docker-compose.yml up -d --build
 
 echo ">> Done!"
+read -n 1 -s -r -p "Press any key to continue..."
+echo
